f679b2c00809d62164af2637c60ba0b77f64a028
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-10-25 13:49:28 -0300

    início da pág de pedidos

--- src/pages/requests/index.js
+++ src/pages/requests/index.js
@@ -1,26 +1,114 @@
-import { React } from 'react';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
+import { React } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
-import WhatsAppButton from '../../components/whatsappButton'
-
 import './style.scss'
 
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import 'firebase/database'
 import firebaseConfig from '../../FirebaseConfig.js'
+
+import SideBar from '../../components/sideBar'
 
 function Requests() {
 
+    const [requestData, setRequestData] = useState([{}]);
+
+    useEffect(() => {
+
+        const userEmail = localStorage.getItem('userEmail')
+
+        if (!firebase.apps.length)
+            firebase.initializeApp(firebaseConfig)
+
+        firebase.database().ref('requests/').get('/requests')
+            .then(function (snapshot) {
+
+                if (snapshot.exists()) {
+
+                    var data = snapshot.val()
+                    var temp = Object.keys(data).map((key) => data[key])
+                    var requestDataTemp = []
+
+                    temp.map((item) => {
+
+                        if (item.userEmail == userEmail)
+                            requestDataTemp.push(item)
+
+                    })
+                    setRequestData(requestDataTemp)
+
+                } else {
+                    console.log("No data available");
+                }
+            })
+
+    }, []);
+
     return (
 
-        <h1>Ola, mundo!</h1>
+        <main>
+
+            <Header />
+
+            <section className="requestsSection">
+
+                <SideBar />
+
+                <div className="requestsData">
+
+                    <h3>Informações dos pedidos</h3>
+
+                    <div>
+
+                        {requestData.map((request) => {
+
+                            return <>
+
+                                {request.listItem != undefined ?
+
+                                    <div className="boxRequestsList">
+
+                                        <h4> <span>id do pedido: {request.id}</span></h4>
+
+                                        {request.listItem.map(item => {
+
+                                            return (
+
+                                                <div>
+
+                                                    <div>
+
+                                                        <b>{item.title} <span>({item.amount}) </span></b>
+                                                        <h4><span>R$ {item.price}</span></h4>
+
+                                                    </div>
+
+                                                </div>
+
+                                            )
+
+                                        })}
+
+                                    </div>
+
+                                    : <p></p>}
+                            </>
+
+                        })}
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            <Footer />
+
+        </main>
 
     )
 
 }
 
-export default Requests
+export default Requests;
